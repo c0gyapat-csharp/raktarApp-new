5ea0e9a5858780d6ec31116c75cb1b5541d1a846
jaydendeveloper <contact.jaydendev@gmail.com>
2025-10-22 13:58:44 +0200

    Shared project created with temporary DTO model

---
+++ src/dotnet/Shared/RaktarAppShared/Models/WarehouseDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaktarAppShared.Models
+{
+    public class WarehouseDto
+    {
+        private readonly int _id;
+        private readonly string _name;
+        private readonly string _country;
+        private readonly string _region;
+        private readonly int _postCode;
+        private readonly string _city;
+        private readonly string _address;
+
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string Country { get; set; } = null!;
+		public string Region { get; set; } = null!;
+		public int PostCode { get; set; }
+		public string City { get; set; } = null!;
+		public string Address { get; set; } = null!;
+
+        public WarehouseDto() { }
+        public WarehouseDto(int id, string name, string country, string region, int postCode, string city, string address)
+        {
+            _id = id;
+            _name = name;
+            _country = country;
+            _region = region;
+            _postCode = postCode;
+            _city = city;
+            _address = address;
+        }
+    }
+}
