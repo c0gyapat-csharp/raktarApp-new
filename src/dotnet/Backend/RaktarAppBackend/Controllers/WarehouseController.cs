b7806705bbb7d87d6615a1122a9f0a72f58592d1
jaydendeveloper <contact.jaydendev@gmail.com>
2025-10-22 12:20:51 +0200

    DB context added to controllers

--- src/dotnet/Backend/RaktarAppBackend/Controllers/WarehouseController.cs
+++ src/dotnet/Backend/RaktarAppBackend/Controllers/WarehouseController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RaktarAppBackend.Context;
 
 namespace RaktarAppBackend.Controllers
 {
@@ -6,6 +7,9 @@ namespace RaktarAppBackend.Controllers
     [Route("[controller]")]
     public class WarehouseController
     {
+
+        AppSqliteDbContext _dbContext = new AppSqliteDbContext();
+
         [HttpGet]
         public IActionResult GetItems()
         {
